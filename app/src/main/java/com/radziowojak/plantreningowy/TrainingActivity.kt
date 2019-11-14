8c2fd07c8b3fb1b9249352226020a00c2b6ee8da
Radosław <radoslaw.kaminski.mail@gmail.com>
2019-11-14 17:02:58 +0100

    Zrobienie frontu pierwszego ćwiczenia w systemie RPT

---
+++ app/src/main/java/com/radziowojak/plantreningowy/TrainingActivity.kt
@@ -0,0 +1,11 @@
+package com.radziowojak.plantreningowy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TrainingActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_training)
+    }
+}
