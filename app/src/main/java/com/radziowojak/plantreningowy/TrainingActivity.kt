d5d9af9cd64134d9c98e18d92d04b63eb6d94c84
Radosław <radoslaw.kaminski.mail@gmail.com>
2019-11-17 23:11:49 +0100

    wypisanie i policzenie wszystkich ciężarów przy wyciskaniu

--- app/src/main/java/com/radziowojak/plantreningowy/TrainingActivity.kt
+++ app/src/main/java/com/radziowojak/plantreningowy/TrainingActivity.kt
@@ -2,6 +2,7 @@ package com.radziowojak.plantreningowy
 
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_training.*
@@ -11,6 +12,9 @@ class TrainingActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_training)
 
+        fun roundAndConvert(number: Float): String {return (Math.round(number/1.25) * 1.25).toString()}
+        fun roundAndConvert(number: Double): String {return (Math.round(number/1.25) * 1.25).toString()}
+
         val sharedprefs = SharedPrefs(this)
         val lastTrainingName = sharedprefs.prefs.getString("training_name", "")
         val editor = sharedprefs.prefs.edit()
@@ -18,6 +22,7 @@ class TrainingActivity : AppCompatActivity(){
         {
             training_name.text = "Trening A"
 
+            /* wyciskanie1 */
             wyciskanie11.text = sharedprefs.prefs.getFloat("wyciskanie11", 0F).toString()
             wyciskanie12.text = sharedprefs.prefs.getFloat("wyciskanie12", 0F).toString()
             wyciskanie13.text = sharedprefs.prefs.getFloat("wyciskanie13", 0F).toString()
@@ -38,7 +43,56 @@ class TrainingActivity : AppCompatActivity(){
                 terazwyciskanie13.setText(((wyciskanie13.text as String).toFloat() + 2.5).toString())
                 terazwyciskanie14.setText(((wyciskanie14.text as String).toFloat() + 2.5).toString())
             }
+            rwyciskanie11.text = roundAndConvert(((terazwyciskanie11.text).toString().toFloat() * 0.5))
+            rwyciskanie12.text = roundAndConvert(((terazwyciskanie12.text).toString().toFloat() * 0.75))
+            rwyciskanie13.text = roundAndConvert(((terazwyciskanie13.text).toString().toFloat() * 0.9))
+            nastronew11.text = roundAndConvert(((((terazwyciskanie11.text).toString().toFloat() * 0.5) - 20) / 2))
+            nastronew12.text = roundAndConvert(((((terazwyciskanie12.text).toString().toFloat() * 0.75) - 20) / 2))
+            nastronew13.text = roundAndConvert(((((terazwyciskanie13.text).toString().toFloat() * 0.9) - 20) / 2))
+            nastronew14.text = roundAndConvert((((terazwyciskanie11.text).toString().toFloat() - 20) / 2))
+            nastronew15.text = roundAndConvert((((terazwyciskanie12.text).toString().toFloat() - 20) / 2))
+            nastronew16.text = roundAndConvert((((terazwyciskanie13.text).toString().toFloat() - 20) / 2))
+            nastronew17.text = roundAndConvert((((terazwyciskanie14.text).toString().toFloat() - 20) / 2))
+            terazwyciskanie11.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {}
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    try {
+                        rwyciskanie11.text = roundAndConvert(((terazwyciskanie11.text).toString().toFloat() * 0.5))
+                        rwyciskanie12.text = roundAndConvert(((terazwyciskanie11.text).toString().toFloat() * 0.75))
+                        rwyciskanie13.text = roundAndConvert(((terazwyciskanie11.text).toString().toFloat() * 0.9))
 
+                        nastronew11.text = roundAndConvert(((((terazwyciskanie11.text).toString().toFloat() * 0.5) - 20) / 2))
+                        nastronew12.text = roundAndConvert(((((terazwyciskanie12.text).toString().toFloat() * 0.75) - 20) / 2))
+                        nastronew13.text = roundAndConvert(((((terazwyciskanie13.text).toString().toFloat() * 0.9) - 20) / 2))
+
+                        nastronew14.text = roundAndConvert((((terazwyciskanie11.text).toString().toFloat() - 20) / 2))
+                    } catch (nfe: NumberFormatException){}
+                }
+            })
+            terazwyciskanie12.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {}
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    try {nastronew15.text = roundAndConvert((((terazwyciskanie12.text).toString().toFloat() - 20) / 2))} catch (nfe: NumberFormatException){}
+                }
+            })
+            terazwyciskanie13.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {}
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    try {nastronew16.text = roundAndConvert((((terazwyciskanie13.text).toString().toFloat() - 20) / 2))} catch (nfe: NumberFormatException){}
+                }
+            })
+            terazwyciskanie14.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {}
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    try {nastronew17.text = roundAndConvert((((terazwyciskanie14.text).toString().toFloat() - 20) / 2))} catch (nfe: NumberFormatException){}
+                }
+            })
+
+            /* wyciskanie2 */
             wyciskanie21.text = sharedprefs.prefs.getFloat("wyciskanie21", 0F).toString()
             wyciskanie22.text = sharedprefs.prefs.getFloat("wyciskanie22", 0F).toString()
             wyciskanie23.text = sharedprefs.prefs.getFloat("wyciskanie23", 0F).toString()
@@ -59,6 +113,54 @@ class TrainingActivity : AppCompatActivity(){
                 terazwyciskanie23.setText(((wyciskanie23.text as String).toFloat() + 2.5).toString())
                 terazwyciskanie24.setText(((wyciskanie24.text as String).toFloat() + 2.5).toString())
             }
+            rwyciskanie21.text = roundAndConvert(((terazwyciskanie21.text).toString().toFloat() * 0.5))
+            rwyciskanie22.text = roundAndConvert(((terazwyciskanie22.text).toString().toFloat() * 0.75))
+            rwyciskanie23.text = roundAndConvert(((terazwyciskanie23.text).toString().toFloat() * 0.9))
+            nastronew21.text = roundAndConvert(((((terazwyciskanie21.text).toString().toFloat() * 0.5) - 20) / 2))
+            nastronew22.text = roundAndConvert(((((terazwyciskanie22.text).toString().toFloat() * 0.75) - 20) / 2))
+            nastronew23.text = roundAndConvert(((((terazwyciskanie23.text).toString().toFloat() * 0.9) - 20) / 2))
+            nastronew24.text = roundAndConvert((((terazwyciskanie21.text).toString().toFloat() - 20) / 2))
+            nastronew25.text = roundAndConvert((((terazwyciskanie22.text).toString().toFloat() - 20) / 2))
+            nastronew26.text = roundAndConvert((((terazwyciskanie23.text).toString().toFloat() - 20) / 2))
+            nastronew27.text = roundAndConvert((((terazwyciskanie24.text).toString().toFloat() - 20) / 2))
+            terazwyciskanie21.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {}
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    try {
+                        rwyciskanie21.text = roundAndConvert(((terazwyciskanie21.text).toString().toFloat() * 0.5))
+                        rwyciskanie22.text = roundAndConvert(((terazwyciskanie21.text).toString().toFloat() * 0.75))
+                        rwyciskanie23.text = roundAndConvert(((terazwyciskanie21.text).toString().toFloat() * 0.9))
+
+                        nastronew21.text = roundAndConvert(((((terazwyciskanie21.text).toString().toFloat() * 0.5) - 20) / 2))
+                        nastronew22.text = roundAndConvert(((((terazwyciskanie22.text).toString().toFloat() * 0.75) - 20) / 2))
+                        nastronew23.text = roundAndConvert(((((terazwyciskanie23.text).toString().toFloat() * 0.9) - 20) / 2))
+
+                        nastronew24.text = roundAndConvert((((terazwyciskanie21.text).toString().toFloat() - 20) / 2))
+                    } catch (nfe: NumberFormatException){}
+                }
+            })
+            terazwyciskanie22.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {}
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    try {nastronew25.text = roundAndConvert((((terazwyciskanie22.text).toString().toFloat() - 20) / 2))} catch (nfe: NumberFormatException){}
+                }
+            })
+            terazwyciskanie23.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {}
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    try {nastronew26.text = roundAndConvert((((terazwyciskanie23.text).toString().toFloat() - 20) / 2))} catch (nfe: NumberFormatException){}
+                }
+            })
+            terazwyciskanie24.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {}
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    try {nastronew27.text = roundAndConvert((((terazwyciskanie24.text).toString().toFloat() - 20) / 2))} catch (nfe: NumberFormatException){}
+                }
+            })
 
             /*
             1razy.text = sharedprefs.prefs.getInt("1razy", 0).toString()
